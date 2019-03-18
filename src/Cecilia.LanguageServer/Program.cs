cb2c1f9741c9748f814feab504c50a26c614f2eb
underdolphin <underdolphin@users.noreply.github.com>
2019-03-19 00:12:53 +0900

    Change runtime to .netcore 3.0

---
+++ src/Cecilia.LanguageServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cecilia.LanguageServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
