bf3f38e934d9f67478bf0afc2d9802cb2132cae6
underdolphin <underdolphin@users.noreply.github.com>
2019-03-30 19:31:56 +0900

    Add Syntax namespace , and move SyntaxKind.cs to that,

--- src/Cecilia.Test/AnalyzerTest/LexerTest.cs
+++ src/Cecilia.Test/AnalyzerTest/LexerTest.cs
@@ -12,6 +12,7 @@
    See the License for the specific language governing permissions and
    limitations under the License. */
 using Cecilia.Lib.Analyzer;
+using Cecilia.Lib.Syntax;
 using Xunit;
 
 namespace Cecilia.Test.AnalyzerTest
