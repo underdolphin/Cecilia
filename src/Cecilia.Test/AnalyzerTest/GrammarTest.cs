6dfa8ad9cfc03f1cdb6dbeecd5c7a0217afecbb5
underdolphin <underdolphin@users.noreply.github.com>
2019-04-12 01:00:32 +0900

    Add identifier parsar.

---
+++ src/Cecilia.Test/AnalyzerTest/GrammarTest.cs
@@ -0,0 +1,29 @@
+﻿/* Copyright 2019 masato sueda
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+using Cecilia.Lib.Analyzer;
+using Sprache;
+using Xunit;
+
+namespace Cecilia.Test.AnalyzerTest
+{
+    public class GrammarTest
+    {
+        [Fact(DisplayName = "IdentifierTest")]
+        public void IdentifierTest()
+        {
+            var ceciliaSrc = "abc123";
+            Assert.Equal(ceciliaSrc, Grammar.Identifier.Parse(ceciliaSrc));
+        }
+    }
+}
