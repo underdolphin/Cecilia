651e067ccdeeac2a814e91fd01fffad4b23de342
underdolphin <underdolphin@users.noreply.github.com>
2019-04-17 00:33:43 +0900

    Add punctions tabke.

---
+++ src/Cecilia.Lib/Analyzer/CeciliaPunctions.cs
@@ -0,0 +1,73 @@
+﻿/* Copyright 2019 masato sueda
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cecilia.Lib.Analyzer
+{
+    public class CeciliaPunctions
+    {
+        public const string LParen = "(";
+        public const string RParen = ")";
+        public const string LBracket = "[";
+        public const string RBracket = "]";
+        public const string LBrace = "{";
+        public const string RBrace = "}";
+        public const string Exclamation = "!";
+        public const string Dollar = "$";
+        public const string Parcent = "%";
+        public const string Caret = "^";
+        public const string Ampersand = "&";
+        public const string Asterisk = "*";
+        public const string Minus = "-";
+        public const string Plus = "+";
+        public const string Equal = "=";
+        public const string Bar = "|";
+        public const string Backslash = "\\";
+        public const string Colon = ":";
+        public const string SemiColon = ";";
+        public const string DoubleQuote = "\"";
+        public const string SingleQuote = "\'";
+        public const string LessThan = "<";
+        public const string GreaterThan = ">";
+        public const string Comma = ",";
+        public const string Dot = ".";
+        public const string Question = "?";
+        public const string Hash = "#";
+        public const string Slash = "/";
+
+        public const string BarBar = "||";
+        public const string AmpAmp = "&&";
+        public const string MinusMinus = "--";
+        public const string PlusPlus = "++";
+        public const string QuestionQuestion = "??";
+        public const string EqualsEquals = "==";
+        public const string ExclamationEquals = "!=";
+        public const string Arrow = "=>";
+        public const string LessThanEqual = "<=";
+        public const string GreaterThanEqual = ">=";
+        public const string LessThanLessThan = "<<";
+        public const string GreaterThanGreaterThan = ">>";
+        public const string PlusEqual = "+=";
+        public const string MinusEqual = "-=";
+        public const string AsteriskEqual = "*=";
+        public const string SlashEqual = "/=";
+        public const string BarEqual = "|=";
+        public const string CaretEqual = "^=";
+
+        public const string LessThanLessThanEqual = "<<=";
+        public const string GreaterThanGreaterThanEqual = ">>=";
+    }
+}
